4a71d703519c23015a4e61396405832ff57a64c6
leeoos <leoanrdo.colosi@gmail.com>
2024-03-23 19:31:59 +0100

    download data for task-24

--- tasks/24/task_24.py
+++ tasks/24/task_24.py
@@ -21,7 +21,7 @@ import pandas as pd
 
 
 global download 
-download = False
+download = True
 
 def progress_bar(current, total, width=80):
   progress_message = "Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total)
