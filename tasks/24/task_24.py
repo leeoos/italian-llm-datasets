655e30d3b55988a612c25dcfceae09f21be4dd0f
leeoos <leoanrdo.colosi@gmail.com>
2024-03-24 19:12:38 +0100

    code for task 24 subtask 2

--- tasks/24/task_24.py
+++ tasks/24/task_24.py
@@ -21,8 +21,6 @@ import csv
 import jsonlines
 import pandas as pd
 
-
-
 def progress_bar(current, total, width=80):
   progress_message = "Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total)
   sys.stdout.write("\r" + progress_message)
@@ -43,6 +41,62 @@ def delete_data():
   except OSError:
       pass
 
+def make_jsonl(output_jsonl, pandas_df, TASK=1, DEBUG=False):
+  with open(output_jsonl, "w",  encoding="utf-8") as jout:
+
+    DEBUG_COUNTER = 0
+    for data in pandas_df.itertuples():
+
+      if DEBUG:
+        print(f"data index: {data.Index}")
+        print(f"data id: {data.idtwitter}")
+        print(f"data subj: {data.subj}")
+        print(f"data subj: {data.opos}")
+        print(f"data subj: {data.oneg}")
+        print(f"data subj: {data.iro}")
+        print(f"data subj: {data.lpos}")
+        print(f"data subj: {data.lneg}")
+        print(f"data subj: {data.top}")
+        print(f"data text: {data.text}")
+
+      if TASK == 1:
+        choices = ["objective", "subjective"]
+        label = 0 if data.subj == 0 else 1
+      
+      elif TASK == 2:
+        choices = ["positive", "negative", "mixed", "neutral"]
+        combos = {
+          (1,0): 0,
+          (0,1): 1,
+          (1,1): 2,
+          (0,0): 3
+        }
+        label = combos[(data.opos, data.oneg)]
+
+      elif TASK == 3:
+        #TODO
+        pass
+    
+      else:
+        raise NameError
+      
+
+      json_dict = {
+        "id": data.idtwitter,
+        "text": data.text,
+        "choices":  choices,
+        "label": label 
+      }
+
+      json_str = json.dumps(json_dict)
+      jout.write(json_str + '\n')
+
+      DEBUG_COUNTER += 1
+      if DEBUG and DEBUG_COUNTER > 1: 
+        break
+
+  print(f"Total data from {train_data} dumped into jsonl: {DEBUG_COUNTER}")
+
 
 if __name__ == '__main__' : 
 
@@ -55,13 +109,13 @@ if __name__ == '__main__' :
 
   if download:
 
-    # download test data
+    # download train data
     train_data_url = "http://www.di.unito.it/~tutreeb/sentipolc-evalita16/training_set_sentipolc16.csv.zip"
     train_data_out = "sentipolc_train_data.zip"
     get_dat_from_url(train_data_url, train_data_out)
     train_data = "training_set_sentipolc16.csv"
 
-    # download train data
+    # download test data
     test_data_url = "http://www.di.unito.it/~tutreeb/sentipolc-evalita16/test_set_sentipolc16_gold2000.csv.zip"
     test_data_out = "sentipolc_test_data.zip"
     get_dat_from_url(test_data_url, test_data_out)
@@ -80,49 +134,25 @@ if __name__ == '__main__' :
 
     # use cached datasel saved in local
     train_data = root_dir + "/data/24/training_set_sentipolc16.csv"
-    test_data = root_dir + "./data/24/test_set_sentipolc16_gold2000.csv"
+    test_data = root_dir + "/data/24/test_set_sentipolc16_gold2000.csv"
 
   print(f"train dataset: {train_data}")
-  print(f"test dataset: {test_data}")
-
   train_df = pd.read_csv(train_data)
-  print(train_df.head)
-  # print(type(train_df))
 
-  # Task 1
+  print(f"test dataset: {test_data}")
+  # test_df = pd.read_csv(test_data)
 
-  # output files 
-  output_jsonl = "haspeede3-task1-train-data.jsonl"
+  # print header just to be on the safe side
+  # print(test_df.head)
 
-  DEBUG = False
-  with open(output_jsonl, "w",  encoding="utf-8") as jout:
+  # insert subtask tag
+  sub_task = 2
 
-    DEBUG_COUNTER = 0
-    for data in train_df.itertuples():
+  train_output_jsonl = "haspeede3-task" + str(sub_task) + "-train-data.jsonl"
+  make_jsonl(train_output_jsonl, train_df, TASK=sub_task, DEBUG=False)
 
-      if DEBUG:
-        print(f"data index: {data.Index}")
-        print(f"data id: {data.idtwitter}")
-        print(f"data text: {data.text}")
-        print(f"data subj: {data.subj}")
-
-      label = 0 if data.subj == 0 else 1
-
-      json_dict = {
-        "id": data.idtwitter,
-        "text": data.text,
-        "choices":  ["objective", "subjective"],
-        "label": label # Make sure 'label' exists in your DataFrame
-      }
-
-      json_str = json.dumps(json_dict)
-      jout.write(json_str + '\n')
-
-      DEBUG_COUNTER += 1
-      if DEBUG and DEBUG_COUNTER > 1: 
-        break
-
-  print(f"Total data from {train_data} dumpet into jsonl: {DEBUG_COUNTER}")
+  # test_output_jsonl = "haspeede3-task" + str(task) + "-test-data.jsonl"
+  # make_jsonl(test_output_jsonl, train_df, TASK=task, DEBUG=False)
 
   # to free up memory and avoid pushing data on github
   if download:
