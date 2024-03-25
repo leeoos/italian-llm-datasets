77dfd3d8d68094cad5046f62dd1d319e56d09eda
leeoos <leoanrdo.colosi@gmail.com>
2024-03-25 23:04:38 +0100

    complete code for task 24 (to test)

--- tasks/24/task_24.py
+++ tasks/24/task_24.py
@@ -24,45 +24,53 @@ import pandas as pd
 # FUNCTIONS
 
 def progress_bar(current, total, width=80):
+  """Plain progress bar to monitor download status"""
+
   progress_message = "Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total)
   sys.stdout.write("\r" + progress_message)
   sys.stdout.flush()
 
 def get_dat_from_url(data_url, data_out):
+    """A function to download the dataset from given url"""
+
     wget.download(url=data_url, out=data_out, bar=progress_bar)    
     print()
     with zipfile.ZipFile(data_out, "r") as zip_ref:
       zip_ref.extractall()
 
 def delete_data():
+  """A simple function used to remove the original dataset file from the pwd after the script execution. This is done to avoid pushing large files on github."""
+
   try:
     os.remove("sentipolc_train_data.zip")
     os.remove("training_set_sentipolc16.csv")
     os.remove("sentipolc_test_data.zip")
     os.remove("test_set_sentipolc16_gold2000.csv")
+
   except OSError:
       pass
   
 def move_data(data, dest="results"):
+  """Just to move the final dtaset in the destination folder and once again avoid pushing large files."""
+
   os.makedirs(dest, exist_ok=True)
   shutil.move(data, dest + "/" + data)
 
+def correct(data, FIXED_LEN=9, DEBUG=False) :
+  """This function fixes the original csv dataset by removing extra quotes that messed up the standard column delimeters."""
 
-def correct(data, DEBUG=False) :
   big_list = []
   with open(data, newline='') as csvfile:
-
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     counter = 0
     bad_formatted = 0
 
     for row in spamreader:
       counter += 1
-
-      if len(row) > 9:
+      if len(row) > FIXED_LEN:
         # removing extra double quotes 
-        new_row = [int(i.strip('"')) for i in row[:8]]
-        new_row.append(''.join(i for i in row[8:])[1:-1])
+        new_row = [int(i.strip('"')) for i in row[:(FIXED_LEN -1)]]
+        new_row.append(''.join(i for i in row[(FIXED_LEN - 1):])[1:-1])
         big_list.append(new_row)
 
         if DEBUG:
@@ -82,23 +90,41 @@ def correct(data, DEBUG=False) :
   print(f"Len after manipulation --> {len(big_list)}")  
   return big_list
 
+def check_quotes(data, FIXED_LEN=9):
+  """ This function  check the presence of double quote inside the tweets and make sure that those are preserved since they are a meaningful communication device. """
+  
+  with open(data, newline='') as csvfile:
+      spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+
+      # check for extra quotes only in the text
+      for row in spamreader:
+        for elem in row[(FIXED_LEN - 1):]:
+          val = '”' in elem
+          if val:
+            print(f"before --> {elem}")
+            new_elem = elem.strip('"') 
+            print(f"after --> {new_elem}", end="\n\n")
+
+
 def check_consistency(data, FIXED_LEN=9):
-  error_counter = 0    
+  """This function is used to verify that no samples are discarded after the correction process."""
+
+  error_counter = 0   
   for d in data:
     if len(d) > FIXED_LEN:
       error_counter += 1
       print(f"Bad sample --> {d}")
 
   print(f"Errors --> {error_counter}")
-
   return error_counter == 0
 
 def make_jsonl(output_jsonl, pandas_df, TASK=1, DEBUG=False):
+  """This is the main function used to generate the desired json dataset in output. This function produce a different output for each given sub-task in [1,2,3]."""
+
   with open(output_jsonl, "w",  encoding="utf-8") as jout:
-
     DEBUG_COUNTER = 0
-    for data in pandas_df.itertuples():
 
+    for data in pandas_df.itertuples():
       if DEBUG:
         print(f"data index: {data.Index}")
         print(f"data id: {data.idtwitter}")
@@ -200,7 +226,9 @@ if __name__ == '__main__' :
   if check_consistency(test_data):
     columns = list(train_df)
     test_df = pd.DataFrame(test_data, columns=columns)
+
     # print header just to be on the safe side
+    check_quotes(train_data)
     print(train_df.head, end="\n")
     print(test_df.head)
     
@@ -209,10 +237,10 @@ if __name__ == '__main__' :
       
   train_output_jsonl = "haspeede3-task" + str(sub_task) + "-train-data.jsonl"
   make_jsonl(train_output_jsonl, train_df, TASK=sub_task, DEBUG=False)
+  move_data(train_output_jsonl, "results")
 
   test_output_jsonl = "haspeede3-task" + str(sub_task) + "-test-data.jsonl"
   make_jsonl(test_output_jsonl, test_df, TASK=sub_task, DEBUG=False)
-
   move_data(test_output_jsonl, "results")
 
   # to avoid pushing data on github
