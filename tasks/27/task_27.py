47b0c3fae8f9c52b7626eddeadc552db6e57363d
RenziPaolo <paolorenzi00.1@gmail.com>
2024-04-02 15:56:41 +0200

    working task 27

--- tasks/27/task_27.py
+++ tasks/27/task_27.py
@@ -15,6 +15,7 @@ import zipfile
 # data manipulations
 import json
 import pandas as pd
+import re
 
 
 # FUNCTIONS
@@ -64,6 +65,8 @@ def format_columns(columns):
     formatted_columns = ['"' + '", "'.join(column.split()) + '"' for column in columns]
     return formatted_columns
 
+
+
 def correct_quotes_in_csv(csv_path):
     """
     Corrects the badly formatted quotes in the CSV file.
@@ -89,81 +92,38 @@ def correct_quotes_in_csv(csv_path):
         return False
 
 
-def make_dataframe(data, columns):
+def make_list(data, columns):
     """This function will generate a pandas dataframe after checking (and correcting) the format of the provided csv."""
     
     print(f"Columns: {columns}")
     print(f"Dataset: {data}")
 
-    to_correct = []
+
     for csv_path in data:
         with open(csv_path, 'r', encoding='utf-8') as file:
-            content = file.read()
-            if '"' in content:
-                to_correct.append(csv_path)
+          whole = file.read()
+    whole = whole.split('"training_politics"\n')
 
-    print(f"Contains bad formatted quotes --> {to_correct}")
+    csv = []
+    for elem in whole[1:]:
+      elem = elem.split('","')
+      if elem == ['']:
+        continue
+      elem[0] = elem[0][1:]
+      elem[2] = elem[2][:-2]
+      csv.append(elem)
+      if len(elem)!=3:
+        print("error!", elem)
 
-    if to_correct:
-        for csv_path in to_correct:
-          # Apply format correction function here
-          for csv_path in to_correct:
-            if correct_quotes_in_csv(csv_path):
-                print(f"Quotes corrected successfully in {csv_path}")
-            else:
-                print(f"Failed to correct quotes in {csv_path}")
+    return csv
 
-        if all(check_consistency(csv_path, column_sublist) for csv_path, column_sublist in zip(data, columns)):
-            dfs = []
-            for csv_path, column_sublist in zip(data, columns):
-                df = pd.read_csv(csv_path, usecols=column_sublist)
-                dfs.append(df)
-            concatenated_df = pd.concat(dfs, ignore_index=True)
-            print(concatenated_df.head())
-            return concatenated_df
-        else:
-            print(f"Error: One or more CSV files are badly formatted.")
-            return None
-    else:
-        dfs = []
-        for csv_path, column_sublist in zip(data, columns):
-            df = pd.read_csv(csv_path, usecols=format_columns(column_sublist))
-            dfs.append(df)
-        concatenated_df = pd.concat(dfs, ignore_index=True)
-        print(concatenated_df.head())
-        return concatenated_df
-
-def df_to_jsonl(output_jsonl, pandas_df, TASK=1, DEBUG=False):
+def list_to_jsonl(output_jsonl, csv, TASK=1, DEBUG=False):
   """This is the main function used to generate the desired json dataset in output. This function produce a different output for each given sub-task in [1,2,3]."""
   print(TASK)
   with open(output_jsonl, "w",  encoding="utf-8") as jout:
     DEBUG_COUNTER = 0
 
-    for data in pandas_df.itertuples():
-      if hasattr(data, 'anonymized_tweet_id'):
-          if DEBUG:
-              print(f"data anonymized_tweet_id: {data.anonymized_tweet_id}")
-              print(f"data created_at: {getattr(data, 'created_at', None)}")
-              print(f"data retweet_count: {getattr(data, 'retweet_count', None)}")
-              print(f"data favorite_count: {getattr(data, 'favorite_count', None)}")
-              print(f"data source: {getattr(data, 'source', None)}")
-              print(f"data is_reply: {getattr(data, 'is_reply', None)}")
-              print(f"data is_retweet: {getattr(data, 'is_retweet', None)}")
-              print(f"data is_quote: {getattr(data, 'is_quote', None)}")
-              print(f"data anonymized_user_id: {getattr(data, 'anonymized_user_id', None)}")
-              print(f"data user_created_at: {getattr(data, 'user_created_at', None)}")
-              print(f"data statuses_count: {getattr(data, 'statuses_count', None)}")
-              print(f"data followers_count: {getattr(data, 'followers_count', None)}")
-              print(f"data friends_count: {getattr(data, 'friends_count', None)}")
-              print(f"data anonymized_description: {getattr(data, 'anonymized_description', None)}")
-              print(f"data dataset: {getattr(data, 'dataset', None)}")
-      elif hasattr(data, 'anonymized_text'):
-          if DEBUG:
-              print(f"data anonymized_tweet_id: {data.anonymized_tweet_id}")
-              print(f"data anonymized_text: {data.anonymized_text}")
-              print(f"data label: {data.label}")
-              print(f"data dataset: {data.dataset}")
-
+    for id, text, label in csv:
       if TASK == 1:
         choices = ["si", "no"]
       
@@ -174,10 +134,10 @@ def df_to_jsonl(output_jsonl, pandas_df, TASK=1, DEBUG=False):
         raise NameError
       
       json_dict = {
-          "id":       int(data.anonymized_tweet_id),
-          "text":     data.anonymized_text,
+          "id":       int(id),
+          "text":     text,
           "choices":  choices,
-          "label":    int(data.label)
+          "label":    int(label)
       }
 
       json_str = json.dumps(json_dict, ensure_ascii=False)
@@ -187,7 +147,7 @@ def df_to_jsonl(output_jsonl, pandas_df, TASK=1, DEBUG=False):
       # if DEBUG and DEBUG_COUNTER > 10: 
       #   break
 
-  print(f"Sub-task --> {TASK} \t Data dumped into jsonl --> {DEBUG_COUNTER}/{len(pandas_df)}")
+  print(f"Sub-task --> {TASK} \t Data dumped into jsonl --> {DEBUG_COUNTER}/{len(csv)}")
 
 
 
@@ -219,9 +179,9 @@ if __name__ == '__main__' :
   training_textual_dev = "./data/development/training_textual.csv"
   training_textual_gold = "./data/gold/test_textual_gold.csv"
 
-  columns_training_contextual = ['anonymized_tweet_id', 'created_at', 'retweet_count', 'favorite_count', 'source', 'is_reply', 'is_retweet', 'is_quote', 'anonymized_user_id', 'user_created_at', 'statuses_count', 'followers_count', 'friends_count', 'anonymized_description', 'dataset']
-  columns_training_textual_dev = ['anonymized_tweet_id', 'anonymized_text', 'label', 'dataset']
-  columns_training_textual_gold = ['anonymized_tweet_id', 'anonymized_text', 'label', 'dataset']
+  #columns_training_contextual = ['anonymized_tweet_id', 'created_at', 'retweet_count', 'favorite_count', 'source', 'is_reply', 'is_retweet', 'is_quote', 'anonymized_user_id', 'user_created_at', 'statuses_count', 'followers_count', 'friends_count', 'anonymized_description', 'dataset']
+  columns_training_textual_dev = ['"anonymized_tweet_id"', '"anonymized_text"', '"label"', '"dataset"']
+  columns_training_textual_gold = ['"anonymized_tweet_id"', '"dataset"', '"label"', '"anonymized_text"']
 
   columns = [columns_training_textual_dev]
   columns_training_textual_gold_list = [columns_training_textual_gold]
@@ -230,7 +190,7 @@ if __name__ == '__main__' :
   training_textual_gold_list = [training_textual_gold]
   json_path = "./data/TAG-it-train.jsonl"
 
-  train_df = make_dataframe(csv_files, columns)
+  train_df = make_list(csv_files, columns)
   sub_task = 1
   train_output_jsonl = "haspeede3-task" + str(sub_task) + "-train-data.jsonl"
-  df_to_jsonl(train_output_jsonl, train_df, TASK=sub_task, DEBUG=True)
+  list_to_jsonl(train_output_jsonl, train_df, TASK=sub_task, DEBUG=False)
