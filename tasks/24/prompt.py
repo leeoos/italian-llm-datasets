a3e31efeec6ebb13421f82becc8fcdf7c4b4dba8
leeoos <leoanrdo.colosi@gmail.com>
2024-04-03 13:23:06 +0200

    dataset evaluation (basic)

--- tasks/24/prompt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-"""TASK 24 - SENTIPOLC 
-
-This script willconvert a sentiment classification dataset taken from http://www.di.unito.it/~tutreeb/sentipolc-evalita16/index.html into a QA dataset siutable for training LLMs"""
