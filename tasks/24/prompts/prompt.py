e862099ec1799fd78713bbe80934777f375473e2
leeoos <leoanrdo.colosi@gmail.com>
2024-04-06 15:04:34 +0200

    prompts for task 24

--- tasks/24/prompts/prompt.py
+++ tasks/24/prompts/prompt.py
@@ -7,6 +7,7 @@ This script automatize the evaluation of different prompts for task 24."""
 import os
 import json
 import random
+import argparse
 
 # read json file
 def load_jsonl(filename):
@@ -21,39 +22,55 @@ def check_data(data):
     print(json_obj["choices"])
     print(json_obj["label"])
 
-data = "../results/sentipolc16-task1-train-data.jsonl"
-# data = "./results/sentipolc16-task1-test-data.jsonl"
-prompts = "sentipolc16-task1-prompt.jsonl"
+def make_prompt(data_entries, selected_prompt_template, index):
+    entry = data_entries[index]
+    print("ID:" + str(entry["id"]))
+    formatted_prompt = selected_prompt_template.replace('{{text}}', entry['text']).replace('{{topic}}', entry['topic'])
+    print(formatted_prompt)
+    print("ANSWER: " + entry["choices"][entry["label"]])
+  
+parser = argparse.ArgumentParser(description='Dataset Manipulation')
+parser.add_argument('--task', '-t', type=int)
+args = parser.parse_args()
+TASK = args.task
 
-# data = "./results/sentipolc16-task2-train-data.jsonl"
-# data = "./results/sentipolc16-task2-test-data.jsonl"
-# prompts = "sentipolc16-task2-prompt.jsonl"
+if TASK == 1:
+  train_data = "../results/sentipolc16-task1-train-data.jsonl"
+  test_data = "../results/sentipolc16-task1-test-data.jsonl"
+  prompts = "sentipolc16-task1-prompt.jsonl"
 
-# data = "./results/sentipolc16-task3-train-data.jsonl"
-# data = "./results/sentipolc16-task3-test-data.jsonl"
-# prompts = "sentipolc16-task3-prompt.jsonl"
+elif TASK == 2:
+  train_data = "../results/sentipolc16-task2-train-data.jsonl"
+  test_data = "../results/sentipolc16-task2-test-data.jsonl"
+  prompts = "sentipolc16-task2-prompt.jsonl"
+
+elif TASK == 3:
+  train_data = "../results/sentipolc16-task3-train-data.jsonl"
+  test_data = "../results/sentipolc16-task3-test-data.jsonl"
+  prompts = "sentipolc16-task3-prompt.jsonl"
+
+else:
+  print(f"Error: no task {TASK}")
+  exit(1)
 
 # load the data and prompt files
-data_entries = load_jsonl(data)
+train_data_entries = load_jsonl(train_data)
+test_data_entries = load_jsonl(test_data)
 prompt_templates = load_jsonl(prompts)
 
-check_data(data_entries)
+# check_data(data_entries)
 
-# for prompt in prompt_templates:
-#   selected_prompt_template = prompt['prompt']
-#   print("\n" + "-"*100 + "\n")
-#   print(prompt)
-#   print()
+random_train_sampels = [random.randint(0, len(train_data_entries)) for i in range(1) ]
+random_test_sampels = [random.randint(0, len(test_data_entries))for i in range(1) ]
 
-#   for i in range(6):
-#     rand_index = random.randint(0, len(data_entries))
-#     entry = data_entries[rand_index]
-#     print(entry)
+for prompt in prompt_templates:
+  selected_prompt_template = prompt['prompt']
+  print("\n" + "-"*100 + "\n")
+  print(f"Template: {prompt['prompt']}")
+  print()
 
-     
-     
-
-# # for each entry in the data file, format the prompt with the data and print it
-# for entry in data_entries:
-#   formatted_prompt = selected_prompt_template.replace('{{text}}', entry['text']).replace('{{topic}}', entry['topic'])
-#   print(formatted_prompt)
+  for train_i, test_i in zip(random_train_sampels, random_test_sampels):
+    print("Train data ", end="")
+    make_prompt(train_data_entries, selected_prompt_template, train_i)
+    print("Test data ", end="")
+    make_prompt(test_data_entries, selected_prompt_template, test_i)
